dd8ec088bbea83ac0588c580e254eeb6a1823d50
Jed Silvan <jedsilvan@gmail.com>
2026-08-06 16:07:43 +0800

    feat: UI updates

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,15 +1,24 @@
 import Pill from './Pill'
 
-export default function Card() {
+export default function Card({ children }: { children?: React.ReactNode }) {
   return (
-    <div className="bg-(--color-card) rounded-lg border-1 border-solid border-border pt-1 pb-3 px-3">
-      <h1 className="text-primary text-base font-medium text-lg">
-        Application Title
-      </h1>
-      <p className="text-secondary text-sm mb-2 font-medium">
-        This is some supporting information for the user.
-      </p>
-      <Pill />
+    <div className="bg-(--color-card) rounded-xl border-1 border-solid border-border pt-1 pb-3 px-3">
+      {children}
     </div>
   )
 }
+
+export function JobCard() {
+  return (
+    <div className="bg-(--color-card) hover:border-brand-blue rounded-xl border-1 border-solid border-border pt-1 pb-3 px-3 cursor-grab">
+        <h1 className="text-primary text-base text-md">
+          Frontend engineer
+        </h1>
+        <p className="text-sm text-tertiary mb-2">
+          Nimbus Labs
+        </p>
+        <Pill />
+    </div>
+  )
+}
+
--- frontend/src/pages/Board.tsx
+++ frontend/src/pages/Board.tsx
@@ -1,7 +1,22 @@
+import { JobCard } from "../components/Card";
+
 export default function Board() {
   return (
-    <div className="container-max-w">
-      <p>Board</p>
+    <div className="container-max-w columns-1 sm:columns-4">
+      {Array.from({ length: 4 }).map((_, index) => (
+        <div key={'card-' + index}>
+          <div className="flex justify-between items-center mb-2">
+            <p className="text-sm font-medium text-secondary">Applied</p>
+            <p className="text-primary/50 text-sm bg-(--color-border)/50 rounded-md px-1 pb-0.5 text-xs">3</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
